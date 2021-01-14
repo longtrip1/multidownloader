601659ea696936ed545b3d9b6debcf156dba27db
longtrip1 <65881086+longtrip1@users.noreply.github.com>
2021-01-14 12:10:47 -0800

    update

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ link = (input('输入链接'))
 Header = input('自定义header,不知道不用填写')
 r=requests.head(link)
 #文件大小
+if 'Accept-Ranges: bytes' in r.headers :
+    pass
+else:
+    print ('链接不支持分块')
 filesize=r.headers['Content-Length']
 block = (filesize / 5)
 #第一个bat
